6e29334337ecf1136784f86282e96f33b2dbff92
marcus96n <marcus_nilsson01@hotmail.com>
2018-11-23 16:16:40 +0100

    Setup Interface

---
+++ src/org/lu/ics/labs/CustomerApplication.java
@@ -0,0 +1,43 @@
+package org.lu.ics.labs;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class CustomerApplication {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CustomerApplication window = new CustomerApplication();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CustomerApplication() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
